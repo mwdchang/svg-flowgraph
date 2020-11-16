da6ea60c2d2da1273e53cccd8cf43840108ebd39
Daniel Chang <mwdchang@gmail.com>
2020-11-16 00:04:25 -0500

    Fix transpiling issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 // import { eslint } from 'rollup-plugin-eslint';
 import pkg from './package.json';
 
@@ -16,6 +17,10 @@ export default [
     plugins: [
       // eslint(),
       nodeResolve(),
+      babel({
+        babelrc: false,
+        presets: [['@babel/preset-env', { modules: false }]]
+      }),
       commonjs(),
       terser()
     ],
@@ -39,6 +44,10 @@ export default [
     plugins: [
       // eslint(),
       nodeResolve(),
+      babel({
+        babelrc: false,
+        presets: [['@babel/preset-env', { modules: false }]]
+      }),
       commonjs(),
       terser()
     ],
