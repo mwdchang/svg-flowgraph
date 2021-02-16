917f6169aca6025e79c297b852e67e2ff2235ede
Kevin Branigan <kbranigan@uncharted.software>
2021-02-16 12:13:54 -0500

    Variables renamed, goal detection moved a bit, slightly better comments

--- src/addons/astar.js
+++ src/addons/astar.js
@@ -3,26 +3,28 @@ import * as d3 from 'd3';
 
 const astar = (G) => {
   /**
-   * A* super basic path search
+   * A* super basic path searching, not terribly optimized but fast enough for me
    *
-   * @param {object} options - highlight options
-   * @param {string} options.color - highlight color
-   * @param {number} options.duration - highlight duration
+   * @param {object} start - coord {x:%, y:%}
+   * @param {string} goal - coord {x:%, y:%}
+   * @param {function} collider - used during pathing, for routing around obstacles
+   * @param {object} grid - optional {x:%, y:%}, default 10,10 - routing is done on this grid
+   * @param {number} searchLimit - this is the max number of grid squares astar will search, default 1000
    */
-
-  const getPath = (originalStart, originalGoal, grid, collider) => {
+  const getPath = (start, goal, collider = () => false, grid = { x:10, y:10 }, searchLimit = 1000) => {
     const pAsKey = (p) => `${p.x},${p.y}`;
     const keyAsP = (key) => ({ x: parseInt(key.split(',')[0]), y: parseInt(key.split(',')[1]) });
     const distance = (p1, p2) => Math.hypot(p1.x - p2.x, p1.y - p2.y);
-    const heuristic = (p) => distance(p, originalGoal) * 1.2;
+    const heuristic = (p) => distance(p, goal) * 1.2;
     const pEqual = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
+
     const nearestValue = (a, v) => Math.round(a / v) * v;
     const nearestOnGrid = (a) => ({ x: nearestValue(a.x, grid.x), y: nearestValue(a.y, grid.y) });
-    const start = nearestOnGrid(originalStart);
-    const goal = nearestOnGrid(originalGoal);
+    const startOnGrid = nearestOnGrid(start);
+    const goalOnGrid = nearestOnGrid(goal);
 
-    if (pEqual(start, goal)) {
-      return [originalStart, originalGoal];
+    if (pEqual(startOnGrid, goalOnGrid)) {
+      return [start, goal];
     }
 
     const openSet = [];
@@ -43,14 +45,14 @@ const astar = (G) => {
         { x: p.x + grid.x, y: p.y - grid.y },
         { x: p.x - grid.x, y: p.y - grid.y },
         { x: p.x - grid.x, y: p.y + grid.y }
-      ].filter(p => !collider(p));
+      ];
     };
 
-    openSet.push(pAsKey(start));
-    gScore[pAsKey(start)] = 0;
-    fScore[pAsKey(start)] = heuristic(start);
+    openSet.push(pAsKey(startOnGrid));
+    gScore[pAsKey(startOnGrid)] = 0;
+    fScore[pAsKey(startOnGrid)] = heuristic(startOnGrid);
 
-    // FIXME: very slow, , but it's not much code and it seems to be fast enough
+    // FIXME: quite slow, but it's not much code and it seems to be fast enough for now
     const getMin = () => {
       let minScore = Number.MAX_VALUE;
       let minKey = '';
@@ -75,20 +77,23 @@ const astar = (G) => {
       const current = keyAsP(currentKey);
       openSet.splice(openSet.indexOf(currentKey), 1);
 
-      if (pEqual(current, goal)) {
-        const path = [currentKey];
-        while (cameFrom[path[path.length - 1]] !== undefined) {
-          path.push(cameFrom[path[path.length - 1]]);
-        }
-        return [].concat([originalStart], path.map(keyAsP).reverse(), [originalGoal]);
-      }
-
-      const neighbours = getNeighbours(current, grid); // thats right, i'm canadian
+      const neighbours = getNeighbours(current, grid); // canadian spelling, sorry not sorry eh
       // getJumpPoints used to be here but not anymore no one knows where it went, i guess it went home
 
       for (let i = 0; i < neighbours.length; i++) {
         const neighbour = neighbours[i];
         const neighbourKey = pAsKey(neighbour);
+
+        if (pEqual(neighbour, goalOnGrid)) {
+          const path = [pAsKey(goal), currentKey];
+          while (cameFrom[path[path.length - 1]] !== undefined) {
+            path.push(cameFrom[path[path.length - 1]]);
+          }
+          path.push(pAsKey(start));
+          return path.map(keyAsP).reverse();
+        }
+
+        if (collider(neighbour)) continue;
 
         const tentativeScore = gScore[pAsKey(current)] + distance(current, neighbour);
         if (gScore[neighbourKey] === undefined || tentativeScore < gScore[neighbourKey]) {
@@ -101,7 +106,7 @@ const astar = (G) => {
         }
       }
     }
-    return [originalStart, originalGoal];
+    return [start, goal];
   }
 
   return [
