ae81082482783046fea05a11140d3bbaf20bb85f
Kevin Branigan <kbranigan@uncharted.software>
2021-02-16 12:16:07 -0500

    Moved functions around, made what I could less-global

--- src/addons/shape.js
+++ src/addons/shape.js
@@ -1,21 +1,19 @@
 
 const shape = (G) => {
-  const getSqDiff = (a, b) => (a - b) * (a - b);
-  const getSqDist = (p1, p2) => getSqDiff(p1.x, p2.x) + getSqDiff(p1.y, p2.y);
-  const distance = (a, b) => Math.hypot(a.x - b.x, a.y - b.y);
-  const pointsalong = (a, b, n) => Array.from(Array(n + 2), (_, i) => ({x: (b.x - a.x) * (i / (n + 1)) + a.x, y: (b.y - a.y) * (i / (n + 1)) + a.y }));
-  const slidingwindow = (arr, n, func) => arr.slice(0, arr.length - n + 1).map((_, i) => func(arr.slice(i, i + n)));
-
-  const getSqSegDist = (p, p1, p2) => {
-    if (p1.x === p2.x && p1.y === p2.y) return getSqDist(p, p1);
-
-    const t = ((p.x - p1.x) * (p2.x - p1.x) + (p.y - p1.y) * (p2.y - p1.y)) / getSqDist(p2, p1);
-    if (t > 1) return getSqDist(p, p2);
-    else if (t > 0) return getSqDist(p, { x: p1.x + (p2.x - p1.x) * t, y: p1.y + (p2.y - p1.y) * t });
-    else return getSqDist(p, p1);
-  }
 
   const simplifyDPStep = (points, first, last, sqTolerance, simplified) => {
+    const getSqSegDist = (p, p1, p2) => {
+      const getSqDiff = (a, b) => (a - b) * (a - b);
+      const getSqDist = (p1, p2) => getSqDiff(p1.x, p2.x) + getSqDiff(p1.y, p2.y);
+
+      if (p1.x === p2.x && p1.y === p2.y) return getSqDist(p, p1);
+
+      const t = ((p.x - p1.x) * (p2.x - p1.x) + (p.y - p1.y) * (p2.y - p1.y)) / getSqDist(p2, p1);
+      if (t > 1) return getSqDist(p, p2);
+      else if (t > 0) return getSqDist(p, { x: p1.x + (p2.x - p1.x) * t, y: p1.y + (p2.y - p1.y) * t });
+      else return getSqDist(p, p1);
+    }
+
     let maxSqDist = sqTolerance, index;
 
     for (let i = first + 1; i < last; i++) {
@@ -47,6 +45,10 @@ const shape = (G) => {
   }
 
   const addPoints = (path, minDistance = 10.0) => {
+    const distance = (a, b) => Math.hypot(a.x - b.x, a.y - b.y);
+    const pointsalong = (a, b, n) => Array.from(Array(n + 2), (_, i) => ({x: (b.x - a.x) * (i / (n + 1)) + a.x, y: (b.y - a.y) * (i / (n + 1)) + a.y }));
+    const slidingwindow = (arr, n, func) => arr.slice(0, arr.length - n + 1).map((_, i) => func(arr.slice(i, i + n)));
+
     return [].concat(...slidingwindow(path, 2, (pair) => {
       if (distance(pair[0], pair[1]) < minDistance) {
         return [pair[0]];
