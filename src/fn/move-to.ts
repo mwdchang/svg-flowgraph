0d30cdb3be01ebf4d36d96c70ec2bf2a4f71c9a1
Daniel Chang <mwdchang@gmail.com>
2022-04-21 13:59:07 -0400

    fix wrong translation

--- src/fn/move-to.ts
+++ src/fn/move-to.ts
@@ -20,8 +20,36 @@ export const moveTo = <V, E>(
   const chart = G.chart;
   const chartSize = G.chartSize;
   const svg = d3.select(G.svgEl);
+  /*
   const width = G.graph.width < chartSize.width ? chartSize.width : G.graph.width;
   const height = G.graph.height < chartSize.height ? chartSize.height : G.graph.height;
+
+  const widthF = G.graph.width < chartSize.width ? chartSize.width / G.graph.width  :  G.graph.width / chartSize.width;
+  const heightF = G.graph.height < chartSize.height ? chartSize.height / G.graph.height  : G.graph.height / chartSize.height;
+
+  let widthTransform = 1;
+  let heightTransform = 1;
+
+  // Calculate offset translation needed to center the node
+  if (G.graph.width < chartSize.width) {
+    // graph is smaller than physical region
+    widthTransform = chartSize.width;
+  } else {
+    // graph is larger than physical region
+    widthTransform = chartSize.width;
+  }
+
+  if (G.graph.height < chartSize.height) {
+    // graph is smaller than physical region
+    heightTransform = chartSize.height;
+  } else {
+    // graph is larger than physical region
+    heightTransform = chartSize.height;
+  }
+  */
+
+  const widthTransform = chartSize.width;
+  const heightTransform = chartSize.height;
 
 
   // t.k = scale, t.x = translateX, t.y = translateY
@@ -47,11 +75,12 @@ export const moveTo = <V, E>(
   const dx = globalX + 0.5 * nodeWidth;
   const dy = globalY + 0.5 * nodeHeight;
 
+  // fixme div 2 (mult 0.5)
   svg.transition().duration(duration).call(
     G.zoom.transform,
     d3.zoomIdentity.translate(0, 0).scale(t.k).translate(
-      -dx + (0.5 * width) / t.k,
-      -dy + (0.5 * height) / t.k
+      -dx + (0.5 * widthTransform) / t.k,
+      -dy + (0.5 * heightTransform) / t.k
     )
   );
 };
